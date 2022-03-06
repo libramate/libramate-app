ae258ff6df73e5b6a21c7ae21f0187470426c60b
hoersamu <samuel@hoera.dev>
2022-03-06 10:57:10 +0100

    💄 add basic layout

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,19 @@
 import { NextPage } from "next";
+import CustomHead from "../../common/components/Head/Head";
+import { NavLayout } from "../../common/components/layout/Layout";
+import SessionValidator from "../../common/components/SessionValidator/SessionValidator";
 
 const Profile: NextPage = () => {
-  return <h1>Settings</h1>;
+  return (
+    <>
+      <SessionValidator />
+      <CustomHead title="Profile" />
+
+      <NavLayout>
+        <h1>Profile</h1>
+      </NavLayout>
+    </>
+  );
 };
 
 export default Profile;
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,7 +1,19 @@
 import { NextPage } from "next";
+import CustomHead from "../../common/components/Head/Head";
+import { NavLayout } from "../../common/components/layout/Layout";
+import SessionValidator from "../../common/components/SessionValidator/SessionValidator";
 
 const Settings: NextPage = () => {
-  return <h1>Settings</h1>;
+  return (
+    <>
+      <SessionValidator />
+      <CustomHead title="Settings" />
+
+      <NavLayout>
+        <h1>Settings</h1>
+      </NavLayout>
+    </>
+  );
 };
 
 export default Settings;
