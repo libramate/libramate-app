eb66bd28b8f02a0def986e4493c407f8b038bd0e
Samuel Höra <samuel@hoera.dev>
2022-04-15 19:30:00 +0200

    🔧 fixed firebase config

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -10,7 +10,7 @@ import {
   GithubAuthProvider,
 } from "firebase/auth";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { connectStorageEmulator, getStorage } from "firebase/storage";
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -33,22 +33,21 @@ const firebaseApp = createFirebaseApp(firebaseConfig);
 
 // Auth exports
 export const auth = getAuth(firebaseApp);
-connectAuthEmulator(auth, "http://localhost:9099");
+if (!process.env.VERCEL_ENV) {
+  connectAuthEmulator(auth, "http://localhost:9099");
+}
 
 // export const googleAuthProvider = new GoogleAuthProvider();
 export const githubProvider = new GithubAuthProvider();
 
 // Firestore exports
 export const firestore = getFirestore(firebaseApp);
-// export const firestore = firebase.firestore();
-// export { firestore };
-// export const serverTimestamp = serverTimestamp;
-// export const fromMillis = fromMillis;
-// export const increment = increment;
-connectFirestoreEmulator(firestore, "localhost", 8080);
+if (!process.env.VERCEL_ENV) {
+  connectFirestoreEmulator(firestore, "localhost", 8080);
+}
 
 // Storage exports
 export const storage = getStorage(firebaseApp);
-export const STATE_CHANGED = "state_changed";
-
-// Helper functions
+if (!process.env.VERCEL_ENV) {
+  connectStorageEmulator(storage, "localhost", 9199);
+}
